f2534e25e0000f68d778bf7ba2c099c088b79321
Jill Wung <jill880829@gmail.com>
2020-07-14 00:16:05 +0800

    Add selfie image to main page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Hello, I am Jill!', 'I am Wei-Shiang!', 'I am a 21-year-old college student:)'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
